f8e302e2af094a8706b1d495cf5b2792bbc9cfb7
Milda Auglyte <auglyte.m@gmail.com>
2023-05-11 15:52:03 +0300

    def prideti darbuotoja() uzbaigta

---
+++ front_gui.py
@@ -0,0 +1,76 @@
+import PySimpleGUI as sg
+from data_end import Darbuotojai
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///data.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class DarbuotojaiGui:
+    def get_data(self):
+        self.darbuotoju_sarasas = session.query(Darbuotojai).all()
+        data = [
+            [item.id, item.vardas, item.pavarde, item.gimimo_data, item.pareigos, item.atlyginimas, item.nuo_kada_dirba]
+            for item in self.darbuotoju_sarasas
+        ]
+        return data
+
+    def __init__(self):
+        data = self.get_data()
+        headers = ['ID', 'Vardas', 'Pavarde', 'Gimimo data', 'Pareigos', 'Atlyginimas', 'Nuo kada dirba']
+        self.table = sg.Table(values=data, headings=headers, auto_size_columns=True, key="-TABLE-")
+        self.layout = [
+            [self.table],
+            [sg.Button('Prideti nauja darbuotoja', key='prideti'), sg.Button('Istrinti darbuotoja', key='istrinti'), sg.Button('Uzdaryti programa')],
+        ]
+        self.window = sg.Window('Darbuotojai', layout=self.layout)
+
+    def run(self):
+        while True:
+            event, values = self.window.read()
+            if event == sg.WINDOW_CLOSED or event == 'Uzdaryti programa':
+                break
+            elif event == 'prideti':
+                darbuotojas = self.prideti_darbuotoja()
+                if isinstance(darbuotojas, Darbuotojai):
+                    self.table.update(values=self.get_data())
+
+    def prideti_darbuotoja(self):
+
+        layout = [
+            [sg.Text('Iveskite varda:'), sg.Input('', key='vardas')],
+            [sg.Text('Iveskite pavarde:'), sg.Input('', key='pavarde')],
+            [sg.Text('Iveskite gimimo data:'), sg.Input('', key='gimimo_data')],
+            [sg.Text('Iveskite pareigas:'), sg.Input('', key='pareigos')],
+            [sg.Text('Iveskite atlyginima:'), sg.Input('', key='atlyginimas')],
+            [sg.Text('Iveskite idarbinimo data:'), sg.Input('', key='nuo_kada_dirba')],
+            [sg.Button("Prideti", key="Prideti"), sg.Button("Atsaukti", key="Atsaukti")]
+        ]
+        window_add = sg.Window("Prideti nauja darbuotoja", layout=layout)
+
+        while True:
+            event, values = window_add.read()
+            if event in (None, "Atsaukti"):
+                window_add.close()
+                return None
+            elif event == "Prideti":
+                try:
+                    darbuotojas = Darbuotojai(
+                        vardas=values["vardas"], 
+                        pavarde=values["pavarde"], 
+                        gimimo_data=values["gimimo_data"], 
+                        pareigos=values["pareigos"], 
+                        atlyginimas=values["atlyginimas"], 
+                        nuo_kada_dirba=values["nuo_kada_dirba"])
+                    session.add(darbuotojas)
+                    session.commit()
+                except Exception as e:
+                    print(e)
+                else:
+                    window_add.close()
+                    return darbuotojas
+    
+
+darbuotojai = DarbuotojaiGui()
+darbuotojai.run()
