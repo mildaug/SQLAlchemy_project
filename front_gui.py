51a17cd50f4267edd0423590865af4337a4c8123
Milda Auglyte <auglyte.m@gmail.com>
2023-05-12 09:52:09 +0300

    nauja data

--- front_gui.py
+++ front_gui.py
@@ -19,22 +19,33 @@ class DarbuotojaiGui:
     def __init__(self):
         data = self.get_data()
         headers = ['ID', 'Vardas', 'Pavarde', 'Gimimo data', 'Pareigos', 'Atlyginimas', 'Nuo kada dirba']
-        self.table = sg.Table(values=data, headings=headers, auto_size_columns=True, key="-TABLE-")
+        self.table = sg.Table(values=data, headings=headers, auto_size_columns=True, key='-TABLE-', enable_events=True)
         self.layout = [
             [self.table],
-            [sg.Button('Prideti nauja darbuotoja', key='prideti'), sg.Button('Istrinti darbuotoja', key='istrinti'), sg.Button('Uzdaryti programa')],
+            [sg.Button('Prideti nauja darbuotoja', key='prideti'), sg.Button('Atnaujinti darbuotojo info', key='atnaujinti'), sg.Button('Istrinti darbuotoja', key='istrinti'), sg.Button('Uzdaryti programa', key='uzdaryti')],
         ]
         self.window = sg.Window('Darbuotojai', layout=self.layout)
 
     def run(self):
         while True:
             event, values = self.window.read()
-            if event == sg.WINDOW_CLOSED or event == 'Uzdaryti programa':
+            if event == sg.WINDOW_CLOSED or event == 'uzdaryti':
                 break
             elif event == 'prideti':
                 darbuotojas = self.prideti_darbuotoja()
                 if isinstance(darbuotojas, Darbuotojai):
                     self.table.update(values=self.get_data())
+            elif event == "istrinti":
+                pasirinkta_eilute = values['-TABLE-']
+                if pasirinkta_eilute:
+                    pasirinkta_eilute = pasirinkta_eilute[0]
+                    indeksas = pasirinkta_eilute
+                    trinamas_darbuotojas = session.query(Darbuotojai).get(self.darbuotoju_sarasas[indeksas].id)
+                    session.delete(trinamas_darbuotojas)
+                    session.commit()
+                    if isinstance(trinamas_darbuotojas, Darbuotojai):
+                        del self.darbuotoju_sarasas[indeksas]
+                        self.table.update(values=self.get_data())
 
     def prideti_darbuotoja(self):
 
@@ -45,24 +56,24 @@ class DarbuotojaiGui:
             [sg.Text('Iveskite pareigas:'), sg.Input('', key='pareigos')],
             [sg.Text('Iveskite atlyginima:'), sg.Input('', key='atlyginimas')],
             [sg.Text('Iveskite idarbinimo data:'), sg.Input('', key='nuo_kada_dirba')],
-            [sg.Button("Prideti", key="Prideti"), sg.Button("Atsaukti", key="Atsaukti")]
+            [sg.Button('Prideti', key='Prideti'), sg.Button('Atsaukti', key='Atsaukti')]
         ]
-        window_add = sg.Window("Prideti nauja darbuotoja", layout=layout)
+        window_add = sg.Window('Prideti nauja darbuotoja', layout=layout)
 
         while True:
             event, values = window_add.read()
-            if event in (None, "Atsaukti"):
+            if event in (None, 'Atsaukti'):
                 window_add.close()
                 return None
-            elif event == "Prideti":
+            elif event == 'Prideti':
                 try:
                     darbuotojas = Darbuotojai(
-                        vardas=values["vardas"], 
-                        pavarde=values["pavarde"], 
-                        gimimo_data=values["gimimo_data"], 
-                        pareigos=values["pareigos"], 
-                        atlyginimas=values["atlyginimas"], 
-                        nuo_kada_dirba=values["nuo_kada_dirba"])
+                        vardas=values['vardas'], 
+                        pavarde=values['pavarde'], 
+                        gimimo_data=values['gimimo_data'], 
+                        pareigos=values['pareigos'], 
+                        atlyginimas=values['atlyginimas'], 
+                        nuo_kada_dirba=values['nuo_kada_dirba'])
                     session.add(darbuotojas)
                     session.commit()
                 except Exception as e:
@@ -70,7 +81,7 @@ class DarbuotojaiGui:
                 else:
                     window_add.close()
                     return darbuotojas
-    
+                
 
 darbuotojai = DarbuotojaiGui()
 darbuotojai.run()
